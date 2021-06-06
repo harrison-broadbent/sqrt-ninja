2402638fb6da1ca703c4108cd57004ff66279564
Harrison Broadbent <harrisonbroadbent@gmail.com>
2021-06-06 21:34:55 +1000

    add vite config to fix netlify build

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+const { resolve } = require("path");
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "about/index.html"),
+      },
+    },
+  },
+};
